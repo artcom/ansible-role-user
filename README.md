75eb9ee581ef6db3f85885239f618e3e119e3003
Stefan Dieterle <stefand@artcom.de>
2020-01-10 11:49:53 +0100

    Add some more tests

--- README.md
+++ README.md
@@ -7,6 +7,19 @@ Available variables are listed below, along with default values `(see defaults/m
 user_name: "{{ ansible_user }}"
 user_password: superSekret
 user_passwordless_sudo: true
+user_authorized_keys: {}
 ```
 
-Deployment user is created with given password, home folder and bash shell. User is added to sudo group and passwordless sudo is configured for that user.
+Deployment user is created with given password, home folder and bash shell. User is added to sudo group and passwordless sudo is configured for that user. Public keys in `user_authorized_keys` are written to user's `~/.ssh/authorized_keys`.
+
+## Test
+### Requirements
+- python >= 3.7
+- vagrant
+- virtualbox
+
+### Run
+```bash
+pip install -r requirements.txt
+molecule test
+```
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,9 +7,41 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_user_home_directory(host):
-    f = host.file('/home/new_user')
+def test_new_user(host):
+    new_user = host.user('new_user')
+    password_hash = {
+        'bionic': '$6$697372$SwS3xavBFqdkllucy7ck7FcI.y8bUWTaCgfQKN4RsiytLTZM'
+        + 'MPeByop2y4dZNZHylOXhFDce3bvX9LQPdvZNR.',
+        'xenial': '$6$679471$V3AhevTzc.0PS9TQqNB3nuCW8kuiuHreJN7/aX6R/7B8oKKw'
+        + 'yPicutfNh1oqoJnfMowhVUo6cUxgJVXkABe/u/'
+    }
 
-    assert f.exists
-    assert f.user == 'new_user'
-    assert f.group == 'new_user'
+    assert new_user.home == '/home/new_user', new_user.home
+    assert new_user.shell == '/bin/bash'
+    assert 'sudo' in new_user.groups
+    assert new_user.password == password_hash[host.system_info.codename]
+
+
+def test_user_passwordless_sudo(host):
+    new_user_sudoers_file = host.file('/etc/sudoers.d/new_user')
+    file_content = 'new_user ALL=(ALL) NOPASSWD:ALL'
+
+    assert new_user_sudoers_file.exists
+    assert new_user_sudoers_file.user == 'root'
+    assert new_user_sudoers_file.group == 'root'
+    assert oct(new_user_sudoers_file.mode) == '0o440'
+    assert new_user_sudoers_file.content_string == file_content
+
+
+def test_user_authorized_keys(host):
+    authorized_keys_file = host.file('/home/new_user/.ssh/authorized_keys')
+    keys = [
+        'ssh-rsa NEWUSERPUBKEY== new_user',
+        'ssh-rsa ADMINUSERPUBKEY== admin_user\n'
+    ]
+    new_user_keys = '\n'.join(keys)
+
+    assert authorized_keys_file.exists
+    assert authorized_keys_file.user == 'new_user'
+    assert authorized_keys_file.group == 'new_user'
+    assert authorized_keys_file.content_string == new_user_keys
