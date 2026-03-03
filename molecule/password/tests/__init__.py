c0413cce2ca310a672c51f8f699192dcf4ec7203
Stefan Dieterle <stefand@artcom.de>
2026-03-03 13:52:52 +0100

    Format python files

--- molecule/password/tests/__init__.py
+++ molecule/password/tests/__init__.py
@@ -1,11 +1,6 @@
 import os
 import sys
+
 sys.path.append(
-    os.path.dirname(
-        os.path.dirname(
-            os.path.dirname(
-                os.path.realpath(__file__)
-            )
-        )
-    )
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 )
