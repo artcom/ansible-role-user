a1817edb62a3885e771204d85049fc283afb4872
Stefan Dieterle <stefand@artcom.de>
2026-03-03 13:41:37 +0100

    Update distro versions

--- molecule/password/tests/test_pw.py
+++ molecule/password/tests/test_pw.py
@@ -2,12 +2,12 @@ from default.common.test_common import *  # noqa: F401,F403
 
 
 def test_new_user_default(host):
-    new_user = host.user('new_user')
+    new_user = host.user("new_user")
     password_hash = {
-        'focal': '$6$822162$fYIIHK7s4Tzh6xgGd9VApwg3SPILq/BgAcB7ZB1PF2LYrVC8h'
-                 '7pJP1D3iFl.235y9w1mmPwz4wzpwoeDguCxj0',
-        'buster': '$6$496895$t/heuOZjcNNJv.LND58guuROWQSl.bZR4xT9jaCVVFsv7o4C'
-                  'wYjcmiBpP8FemyrCKb1MuAjuWtpcYdftsQLLJ1'
+        "noble": "$6$rounds=656000$808553$TJTSMegyZQiehjaAn3UuVhMdKgAhWm2E6MV."
+        "EqjRh4wSNHW7Fn0ZfKL.iJ2rYs6VzV/NyYMi0oqKUEVdyAaee0",
+        "trixie": "$6$rounds=656000$164790$TI8NxQicQ4fcU.qGsFJUZqr1Futpd6FNDnt"
+        "za0aiGkyvJijKciS3pJuk4cf9UR9yt3IUtc1Oh/SVY9pAM9Isi.",
     }
 
     assert new_user.password == password_hash[host.system_info.codename]
